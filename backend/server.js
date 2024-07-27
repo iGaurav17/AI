fdd09185509bc68459188d7da09d1e29344508b0
iGaurav17 <igauravbhardwaj17@gmail.com>
2024-07-27 16:39:52 +0530

    Initialise git repo for project

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 6001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
